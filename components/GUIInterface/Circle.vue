9271f1c2b8b64ed256f4df2008edf55e03342c5c
webothdo <joshuaogboloo@gmail.com>
2024-03-12 14:08:24 +0100

    modified circle component

--- components/GUIInterface/Circle.vue
+++ components/GUIInterface/Circle.vue
@@ -3,46 +3,11 @@
 
 const controls = useControlsStore()
 
-const circlecontrols = controls?.circleControl
-
-for (let index = 0; index < circlecontrols?.length; index++) {
-    console.log({ ...circlecontrols[index] })
-}
-
-const newObj = circlecontrols.map((control) => {
-    return {
-        ...control
-    }
-})
-
-console.log(newObj[0].label, newObj[0].value)
-
-const properties = ref({
-    label: 'my label',
-    value: 2
-})
-
-watch(() => circlecontrols, () => {
-
-    circlecontrols.map((control) => {
-        console.log(control.label, control.value)
-        console.log(newObj[0].label, newObj[0].value)
-    })
-}, {
-    deep: true
-})
-
-console.log(properties.value.value)
 </script>
 
 <template>
-    <div v-for="(value, index) in controls.circleValue"
+    <div v-for="(value, index) in controls.circleValue" :key="index"
         class="flex justify-between items-center font-[Poppins] gap-1 my-3">
         <GUIComponentInput :model="value" />
     </div>
-    <!-- <div v-for="(control, index) in circlecontrols" :key="index"
-        class="flex justify-between items-center font-[Poppins] gap-1 my-3">
-        <p>{{ controls.circleValue.color }}</p>
-        <GUIComponentInput :model="control" />
-    </div> -->
 </template>
--- stores/useControlsStore.js
+++ stores/useControlsStore.js
@@ -22,41 +22,8 @@ export const useControlsStore = defineStore("controls", () => {
     },
   });
 
-  const circleControl = ref([
-    {
-      label: "cx",
-      value: 50,
-      type: "number",
-    },
-    {
-      label: "cy",
-      value: 50,
-      type: "number",
-    },
-    {
-      label: "radius",
-      value: 45,
-      type: "number",
-    },
-    {
-      label: "color",
-      value: "#fafafa",
-      type: "color",
-    },
-  ]);
-
-  const updateCircleValue = (value) => {
-    circleValue.value = value;
-    console.log("circleValue", circleValue.value);
-  };
-
-  const updateControls = (value) => {};
-
   return {
     circleValue,
-    circleControl,
-    updateCircleValue,
-    updateControls,
   };
 });
 
